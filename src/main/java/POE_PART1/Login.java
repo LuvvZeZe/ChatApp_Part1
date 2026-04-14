f2c9c6dd0ba3e11064d4fff7c95b7e6150cede90
LuvvZeZe <zs.bensah@gmail.com>
2026-04-14 20:42:31 +0200

    added user registration logic
    Created registerUser method that validates username, password, and cell phone number and returns appropriate messages.

--- src/main/java/POE_PART1/Login.java
+++ src/main/java/POE_PART1/Login.java
@@ -66,4 +66,20 @@ public boolean checkCellPhoneNumber() {
 
     return true;
 }
+public String registerUser() {
+
+    if (!checkUserName()) {
+        return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+    }
+
+    if (!checkPasswordComplexity()) {
+        return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+    }
+
+    if (!checkCellPhoneNumber()) {
+        return "Cell phone number incorrectly formatted or does not contain international code.";
+    }
+
+    return "Username successfully captured.\nPassword successfully captured.\nCell phone number successfully added.";
+}
 }
