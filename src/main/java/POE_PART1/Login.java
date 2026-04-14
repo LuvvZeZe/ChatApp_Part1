0006c1978b317a6266b0d776463e11f59878c16c
LuvvZeZe <zs.bensah@gmail.com>
2026-04-14 20:36:57 +0200

    Added password complexity validation
    Implemented password validation to ensure minimum length, uppercase letter, number, and special character.

--- src/main/java/POE_PART1/Login.java
+++ src/main/java/POE_PART1/Login.java
@@ -22,4 +22,25 @@ public class Login
     public boolean checkUserName() {
     return username.contains("_") && username.length() <= 5;
 }
+public boolean checkPasswordComplexity() {
+    if (password == null || password.length() < 8) {
+        return false;
+    }
+
+    boolean hasUpperCase = false;
+    boolean hasDigit = false;
+    boolean hasSpecialChar = false;
+
+    for (char c : password.toCharArray()) {
+        if (Character.isUpperCase(c)) {
+            hasUpperCase = true;
+        } else if (Character.isDigit(c)) {
+            hasDigit = true;
+        } else if (!Character.isLetterOrDigit(c)) {
+            hasSpecialChar = true;
+        }
+    }
+
+    return hasUpperCase && hasDigit && hasSpecialChar;
+}
 }
